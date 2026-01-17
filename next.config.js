d588021bd5634ef0f186bacc3cef206c710288a5
Ishaan_Sawant <imsawant05@gmail.com>
2026-01-18 03:02:33 +0530

    fix: disable turbopack

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
-
   webpack: (config) => config,
-};
-
-module.exports = withPWA(nextConfig);
+});
